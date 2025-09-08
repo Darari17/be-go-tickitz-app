7bd78793c44469c761ba3f67f861e33ebfe5409c
farid <faridramadhan59@gmail.com>
2025-09-08 07:39:43 +0700

    feat: add middlewares

---
+++ internal/middlewares/verifyToken.go
@@ -0,0 +1,48 @@
+package middlewares
+
+import (
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/Darari17/be-go-tickitz-app/pkg"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func VerifyToken(ctx *gin.Context) {
+	bearerToken := ctx.GetHeader("Authorization")
+	if bearerToken == "" || !strings.HasPrefix(bearerToken, "Bearer ") {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"success": false,
+			"error":   "Silahkan login terlebih dahulu",
+		})
+		return
+	}
+
+	token := strings.TrimPrefix(bearerToken, "Bearer ")
+
+	var claims pkg.Claims
+	if err := claims.VerifyToken(token); err != nil {
+		switch err {
+		case jwt.ErrTokenInvalidIssuer, jwt.ErrTokenExpired:
+			log.Println("JWT Error.\nCause:", err.Error())
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"success": false,
+				"error":   "Silahkan login kembali",
+			})
+			return
+		default:
+			log.Println("Internal Server Error.\nCause:", err.Error())
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"success": false,
+				"error":   "Internal Server Error",
+			})
+			return
+		}
+	}
+
+	ctx.Set("user_id", claims.UserId)
+	ctx.Set("role", claims.Role)
+	ctx.Next()
+}
